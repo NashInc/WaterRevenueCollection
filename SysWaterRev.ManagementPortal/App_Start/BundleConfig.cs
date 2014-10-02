1040c4d2c5bbb587b14dd816fc127fc7a42a9d6f
George Mbuthia <mbuthiagrg@gmail.com>
2014-10-02 23:51:44 +0300

    Corrections to bundle config and the site.css file to load without minification.

--- SysWaterRev.ManagementPortal/App_Start/BundleConfig.cs
+++ SysWaterRev.ManagementPortal/App_Start/BundleConfig.cs
@@ -8,23 +8,11 @@ namespace SysWaterRev.ManagementPortal
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"
+                "~/Scripts/jquery-2.1.1.js"
                 ));
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                "~/Scripts/jquery-ui-{version}.js"
-                ));
-            bundles.Add(new ScriptBundle("~/bundles/jqueryplugins").Include(
-                "~/Scripts/jquery.sparkline-{version}.js",
-                "~/Scripts/jquery.knob.js",
-                "~/Scripts/jquery-jvectormap-1.2.2.js",
-                "~/Scripts/jVectorMaps/world-ISO.A3/jquery-jvectormap-world-mill-en-US.js"
-                ));
-            bundles.Add(new ScriptBundle("~/bundles/pickers").Include(
-                "~/Content/template/js/plugins/daterangepicker/daterangepicker.js",
-                "~/Content/template/js/plugins/datepicker/bootstrap-datepicker.js",
-                "~/Content/template/js/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js",
-                "~/Content/template/js/plugins/iCheck/icheck.min.js"
-                ));
+                "~/Scripts/jquery-ui-1.11.1.js"
+                ));          
             bundles.Add(new ScriptBundle("~/bundles/morris").Include(
                 "~/Scripts/morris.js"
                 ));
@@ -32,7 +20,8 @@ namespace SysWaterRev.ManagementPortal
                 "~/Scripts/raphael.js"
                 ));
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
+                "~/Scripts/jquery.validate*"
+                ));
 
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                 "~/Scripts/modernizr-{version}.js"));
@@ -46,8 +35,7 @@ namespace SysWaterRev.ManagementPortal
                 "~/Scripts/kendo/2014.2.903/kendo.all.min.js",
                 "~/Scripts/kendo/2014.2.903/kendo.aspnetmvc.min.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/template").Include(
-                "~/Scripts/icheck.js",
+            bundles.Add(new ScriptBundle("~/bundles/template").Include(              
                 "~/Scripts/app.js",
                 "~/Scripts/demo.js"
                 ));
@@ -56,20 +44,11 @@ namespace SysWaterRev.ManagementPortal
             bundles.Add(new StyleBundle("~/Content/css").Include(
                 "~/Content/bootstrap.css",
                 "~/Content/font-awesome.css",
-                "~/Content/ionicons.css",
-                "~/Content/Site.css"
-                ));
-
-            bundles.Add(new StyleBundle("~/Content/kendo").Include(
-                "~/Content/kendo/2014.2.903/kendo.common.min.css",
-                "~/Content/kendo/2014.2.903/kendo.mobile.all.min.css",
-                "~/Content/kendo/2014.2.903/kendo.dataviz.min.css",
-                "~/Content/kendo/2014.2.903/kendo.bootstrap.min.css",
-                "~/Content/kendo/2014.2.903/kendo.dataviz.bootstrap.min.css"
-                ));
+                "~/Content/ionicons.css"                
+                ));           
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
-            BundleTable.EnableOptimizations = false;
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
